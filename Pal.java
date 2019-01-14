79e4a259d954ee7d26333c8b19a6805a5a5f16c8
ombra1609 <46604978+ombra1609@users.noreply.github.com>
2019-01-14 19:44:35 +0300

    Add files via upload

---
+++ Pal.java
@@ -0,0 +1,5 @@
+public class Pal
+ {
+    public static void main(String[] args) {System.out.println(isPalindrome("Енот"));   
+        System.out.println(isPalindrome("тут"));
+ }}
---
+++ Prost.java
@@ -0,0 +1,7 @@
+public class Prost {
+    public static void main(String[] args) {
+        for (int i = 0; i < 100; i++) {
+            if (isPrime(i))
+                System.out.println(i);
+        }
+    }
