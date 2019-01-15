0734a5ccc1c4ce93c02db6eb8b1383e54a40a140
ombra1609 <46604978+ombra1609@users.noreply.github.com>
2019-01-15 14:08:38 +0300

    Delete prost.java

--- prost.java
+++
@@ -1,7 +0,0 @@
-public class Prost {
-    public static void main(String[] args) {
-        for (int i = 0; i < 100; i++) {
-            if (isPrime(i))
-                System.out.println(i);
-        }
-    }
