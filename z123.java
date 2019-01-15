b6681173d6a83d0ce6cb58c9e7ff2b2a8ac98646
ombra1609 <46604978+ombra1609@users.noreply.github.com>
2019-01-15 14:01:37 +0300

    Add files via upload
    z123 - задания 1,2,3

---
+++ z123.java
@@ -0,0 +1,17 @@
+public class z123 {
+   public static void main(String[] args) {
+	   //задание 1
+	System.out.println("Привет мир!");
+	   //задание 2
+	System.out.println(isPalindrome("енот"));
+	System.out.println(isPalindrome(“обобо"));
+	   //задание 3
+       for (int i = 0; i < 100; i++) {
+       if (isPrime(i))
+       System.out.println(i);
+       }}
+	public static boolean isPalindrome(String string) {
+		return new StringBuilder(string).reverse().toString().equals(string);}
+	public static boolean isPrime(final int num) {
+		return num > 1 && IntStream.range(2, num).noneMatch(index -> num % index == 0);
+   }}
